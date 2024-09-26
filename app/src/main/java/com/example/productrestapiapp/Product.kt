abe1a6c5bf6b11562786e397a56b75a3beed02ac
arafat-jahan <ajh.official15@gmail.com>
2024-09-26 14:06:35 +0600

    Initial commit: Set up project structure and README

---
+++ app/src/main/java/com/example/productrestapiapp/Product.kt
@@ -0,0 +1,13 @@
+package com.example.productrestapiapp
+
+
+data class Product(
+    val images:List<String>,
+    val title:String,
+    val price:Double,
+    val id:Double,
+    val description:String
+
+)
+
+
